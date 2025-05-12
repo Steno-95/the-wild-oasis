715b183f317b7c72f3d526f6afbd3224b454a835
Steno-95 <lezzistefano95@gmail.com>
2025-05-12 15:02:40 +0200

    Feat: create filter menu

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,4 +1,6 @@
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
+import SortBy from "./SortBy";
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -15,7 +17,7 @@ const FilterButton = styled.button`
   border: none;
 
   ${(props) =>
-    props.active &&
+    props.$active &&
     css`
       background-color: var(--color-brand-600);
       color: var(--color-brand-50);
@@ -33,3 +35,58 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
+  function handleClick(value) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleClick(option.value)}
+          $active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+
+      <SortBy
+        options={[
+          {
+            value: "name-asc",
+            label: "Sort by name (A-Z)",
+          },
+          {
+            value: "name-desc",
+            label: "Sort by name (Z-A)",
+          },
+          {
+            value: "regularPrice-asc",
+            label: "Sort by price (low first)",
+          },
+          {
+            value: "regularPrice-desc",
+            label: "Sort by price (high first)",
+          },
+          {
+            value: "maxCapacity-asc",
+            label: "Sort by capacity (low first)",
+          },
+          {
+            value: "maxCapacity-desc",
+            label: "Sort by capacity (high first)",
+          },
+        ]}
+      />
+    </StyledFilter>
+  );
+}
+
+export default Filter;
