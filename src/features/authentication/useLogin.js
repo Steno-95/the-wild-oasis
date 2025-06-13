a1998d99afe014fb1e3f0d2c63d9303f7c945c91
Steno-95 <lezzistefano95@gmail.com>
2025-06-13 13:52:29 +0200

    Fix: add replace option to navigate

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (loginData) => {
       queryClient.setQueriesData(["user"], loginData.user);
-      navigate("/dashboard");
+      navigate("/dashboard", { replate: true });
     },
     onError: (err) => {
       console.log("ERROR", err);
