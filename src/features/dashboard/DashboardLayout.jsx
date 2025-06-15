04e523aa28246aef68826e6c2da94f12ad73baa9
Steno-95 <lezzistefano95@gmail.com>
2025-06-15 17:23:51 +0200

    Feat: add dashboard basic layout and filter menu

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistic</div>
+      <div>Today's Activity</div>
+      <div>Chart stay duration</div>
+      <div>Chart sale</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
