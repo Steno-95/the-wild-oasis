aae441ad2f21833a91c76aa788bef95d4dd676f8
Steno-95 <lezzistefano95@gmail.com>
2025-06-28 15:55:50 +0200

    Feat: add credentials

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,12 +13,44 @@ const LoginLayout = styled.main`
   background-color: var(--color-grey-50);
 `;
 
+const CredentialDisplay = styled.aside`
+  background-color: var(--color-blue-700);
+  padding: 2rem;
+  border: 4px solid var(--color-indigo-100);
+  position: absolute;
+  bottom: 10%;
+  right: 5%;
+`;
+
+const CredentialLayout = styled.div`
+  display: flex;
+  gap: 1.5rem;
+`;
+const CredentialField = styled.p`
+  text-transform: capitalize;
+  color: var(--color-grey-50);
+  font-weight: 600;
+`;
+const CredentialValue = styled.span`
+  color: var(--color-grey-200);
+`;
+
 function Login() {
   return (
     <LoginLayout>
       <Logo />
       <Heading $as="h4">Log in to your account</Heading>
       <LoginForm />
+      <CredentialDisplay>
+        <CredentialLayout>
+          <CredentialField>email address:</CredentialField>
+          <CredentialValue>jonas@example.com</CredentialValue>
+        </CredentialLayout>
+        <CredentialLayout>
+          <CredentialField>password:</CredentialField>
+          <CredentialValue>pass0897</CredentialValue>
+        </CredentialLayout>
+      </CredentialDisplay>
     </LoginLayout>
   );
 }
